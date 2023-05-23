d7a89d8b3af4dae9a3d6f4bfcd7cc75cd2e6f656
EdwanNidzar <56621669+EdwanNidzar@users.noreply.github.com>
2023-05-23 14:54:55 +0800

    Pertemuan1

---
+++ application/controllers/Home.php
@@ -0,0 +1,13 @@
+<?php
+class Welcome extends CI_Controller
+{
+
+    public function index()
+    {
+        $data['title'] = "Dashboard | SIMDAWA-APP";
+        $this->load->view('tamplate/header', $data);
+        $this->load->view('tamplate/sidebar');
+        $this->load->view('home_view');
+        $this->load->view('tamplate/footer');
+    }
+}
